5275e8d1f526940a949156598f8f555fe2af0a0f
msdegt <msdegt@yandex.ua>
2016-03-07 01:30:56 +0200

    ColorProperty

---
+++ Работы по C#/Самостоятельная работа №04. C#/ColorConsole/ColorConsole/ColorPropertyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorConsole
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class ColorPropertyAttribute : System.Attribute
+    {
+        public ColorPropertyAttribute(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        public ConsoleColor Color { get; private set; }
+    }
+}
---
+++ Работы по C#/Самостоятельная работа №04. C#/ColorConsole/ColorConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("BMW", "yellow", 50000);
+            Dog dog = new Dog("Lokky", 1, 23.5);
+
+            Console.WriteLine("Свойства объекта " + car.GetType() + " :");
+            ColorProperty.ColorPrint(car);
+            Console.WriteLine("\n");
+            Console.WriteLine("Свойства объекта " + dog.GetType() + " :");
+            ColorProperty.ColorPrint(dog);
+        }
+    }
+}
